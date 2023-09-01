fe73654b309b9fd2c35275e32f4cc829a1caeac2
nurulhidayyah <nurulhidayyah45@gmail.com>
2023-09-02 03:12:07 +0700

    modifikasi view admin/index

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -23,7 +23,7 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalPengapuran; ?></div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-download fa-2x text-gray-300"></i>
+                                    <i class="fas fa-balance-scale fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
@@ -40,7 +40,7 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalLuasLahan; ?></div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-envelope fa-2x text-gray-300"></i>
+                                    <i class="fas fa-ruler fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
