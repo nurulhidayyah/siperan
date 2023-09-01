766e8cee04d0934af83fa1be2fd4c8b9ab5375ef
nurulhidayyah <nurulhidayyah45@gmail.com>
2023-09-01 09:09:50 +0700

    modifikasi laporan

--- application/views/admin/laporan.php
+++ application/views/admin/laporan.php
@@ -1,48 +1,42 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
-    <?php if (!empty($laporan)) : ?>
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary"><?= $title; ?></h6>
-            </div>
-            <div class="card-body">
-                <a target="_blank" href="<?= base_url('admin/generate_laporan') ?>" class="btn btn-primary mt-2">Preview or Download</a>
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><?= $title; ?></h6>
+        </div>
+        <div class="card-body">
+            <a target="_blank" href="<?= base_url('admin/generate_laporan') ?>" class="btn btn-primary mt-2">Preview or Download</a>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th>Nama Tempat</th>
+                            <th>Luas Lahan</th>
+                            <th>Kadar Keasaman</th>
+                            <th>Jumlah Pengapuran</th>
+                            <th>Bulan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach ($laporan as $l) : ?>
                             <tr>
-                                <th scope="col">#</th>
-                                <th>Nama Tempat</th>
-                                <th>Luas Lahan</th>
-                                <th>Kadar Keasaman</th>
-                                <th>Jumlah Pengapuran</th>
-                                <th>Bulan</th>
+                                <th scope=" row" id="number"><?= $i; ?></th>
+                                <td><?= $l['place_name']; ?></td>
+                                <td><?= $l['surfac_area']; ?></td>
+                                <td><?= $l['ph']; ?></td>
+                                <td><?= $l['score']; ?></td>
+                                <td><?= date('d-m-Y', strtotime($l['created_at'])); ?></td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <?php $i = 1; ?>
-                            <?php foreach ($laporan as $l) : ?>
-                                <tr>
-                                    <th scope=" row" id="number"><?= $i; ?></th>
-                                    <td><?= $l['place_name']; ?></td>
-                                    <td><?= $l['surfac_area']; ?></td>
-                                    <td><?= $l['ph']; ?></td>
-                                    <td><?= $l['score']; ?></td>
-                                    <td><?= date('d-m-Y', strtotime($l['created_at'])); ?></td>
-                                </tr>
-                                <?php $i++; ?>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
-
-    <?php else : ?>
-        <p class="text-center">Belum ada data</p>
-    <?php endif; ?>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
